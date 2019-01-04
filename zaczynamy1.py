5a8279483432d924de92416ecef3a3bcd88d5bf5
mixmg <46382201+mixmg@users.noreply.github.com>
2019-01-04 21:25:51 +0100

    Add files via upload

---
+++ zaczynamy1.py
@@ -0,0 +1,13 @@
+golden = (1 + 5 ** 0.5) / 2
+print(golden)
+
+
+
+def main():
+    a, b = 0,1
+    print(0)
+    while b <= 10000000000000000000000000000000000000000:
+        print(b)
+        a, b = b, a + b
+
+print(main())
